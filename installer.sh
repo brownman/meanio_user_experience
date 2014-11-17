161df560540779eab4ee781cc25cafe38f91f1ed
brownman <brownman556@gmail.com>
2014-11-17 17:29:26 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -12,8 +12,8 @@ grunt &
 curl 0.0.0.0:3000
 
 
-git clone --depth=1 https://github.com/brownman/scrapping.git 
-cd scrapping
+#git clone --depth=1 https://github.com/brownman/scrapping.git 
+#cd scrapping
 #install:
 npm install -g casperjs
 #script:
