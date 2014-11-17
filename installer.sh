ba063a6059adc09ef6c414a4847c19a3509e8056
brownman <brownman556@gmail.com>
2014-11-17 15:16:03 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,6 +1,10 @@
-npm install -g meanio
-mean init
-cd mean
-npm install -g
-npm link
-grunt
+#!/usr/bin/env bash 
+echo 'http://learn.mean.io/'
+
+   npm install -g mean-cli
+   mean init myApp
+   cd myApp
+   #npm -g install
+   npm install -g
+   npm link
+   grunt
