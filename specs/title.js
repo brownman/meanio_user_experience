39c72c1668d9b3d8162b8dfe53185e33c85fb1b6
brownman <brownman556@gmail.com>
2014-11-18 10:08:24 +0200

    removed commented code
    which apparently been executed by the casper test
     :0

--- specs/title.js
+++ specs/title.js
@@ -2,22 +2,9 @@
 casper.test.begin('Google search retrieves 10 or more results', 5, function suite(test) {
     casper.start("http://0.0.0.0:3000", function() {
         test.assertTitle("MEAN - FullStack JS - Development - MEAN - FullStack JS - Development", "should show the mean.io default title");
-       /* 
-       test.assertExists('form[action="/search"]', "main form is found");
-        this.fill('form[action="/search"]', {
-            q: "casperjs"
-        }, true);
-        */
+    
     });
-/*
-    casper.then(function() {
-        test.assertTitle("casperjs - Recherche Google", "google title is ok");
-        test.assertUrlMatch(/q=casperjs/, "search term has been submitted");
-        test.assertEval(function() {
-            return __utils__.findAll("h3.r").length >= 10;
-        }, "google search for \"casperjs\" retrieves 10 or more results");
-    });
-*/
+ 
     casper.run(function() {
         test.done();
     });
