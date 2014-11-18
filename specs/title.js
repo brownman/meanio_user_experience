98eada4671a9f602d638d7b5d7c519b37bd24075
brownman <brownman556@gmail.com>
2014-11-18 10:27:19 +0200

    casper: hard-coded the number of tests

--- specs/title.js
+++ specs/title.js
@@ -1,7 +1,7 @@
 // googletesting.js
-casper.test.begin('Google search retrieves 10 or more results', 5, function suite(test) {
+casper.test.begin('title should be Mean-ingful', 1, function suite(test) {
     casper.start("http://0.0.0.0:3000", function() {
-        test.assertTitle("MEAN - FullStack JS - Development - MEAN - FullStack JS - Development", "should show the mean.io default title");
+        test.assertTitle("MEAN - FullStack JS - Development - MEAN - FullStack JS - Development", "test the title");
     
     });
  
