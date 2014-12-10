872d38991b271c57f72a23ba5ade59decd6f12c8
brownman <brownman556@gmail.com>
2014-12-10 11:25:48 +0200

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,38 +1,41 @@
 #!/bin/bash 
-ver=0.5
+#ver=0.5
 echo 'http://learn.mean.io/'
 
 install(){
  echo install npm deps
- npm install -g mean-cli@${ver}
+ npm install -g mean-cli
+ #@${ver}
  sudo apt-get install curl
 }
 
 scaffold(){
- echo scaffold new skeleton for a mean app
-  
-  
- mean init myApp
- cd myApp
- #npm -g install
- npm install -g
- npm link
+echo 1>&2 scaffold new skeleton for a mean app
+cd $HOME
+/usr/bin/yes |  mean init myApp
+mv y myApp
+cd myApp
+#npm -g install
+npm install -g
+npm link
 }
 
 
 
  test(){
-   grunt &
  }
  
 visit(){
+   grunt &
+
 while true; do  curl 0.0.0.0:3000 2>/dev/null && break ; sleep 1 ; done
 }
  
  steps(){
   install 
   scaffold
-  test
+  grunt test
+  
   visit
  }
 
